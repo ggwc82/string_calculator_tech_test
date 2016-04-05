0828013c88cbd63f4942b022c01df52679d571b6
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 11:16:32 +0100

    Passed third test for adding two string numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,10 @@
 class Calculator
   def add(input)
-    input.to_i
+    if input.length < 2
+      input.to_i
+    else
+      temp_array = input.split(",")
+      temp_array[0].to_i + temp_array[1].to_i
+    end
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'calculator'
 
 describe Calculator do
-  
+
   subject(:calculator) { Calculator.new }
   
   describe '#add' do
@@ -11,6 +11,10 @@ describe Calculator do
 
     it 'will return an integer for a single string number' do
       expect(calculator.add("8")).to eq(8)
+    end
+
+    it 'will return the sum as an integer for two string numbers' do
+      expect(calculator.add("8,2")).to eq(10)
     end
   end
 end
