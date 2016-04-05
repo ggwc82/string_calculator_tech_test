31151c5375f657ccc6ab7a3e917205f89d21a3db
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 10:56:06 +0100

    First test written. Created Calculator class

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,10 @@
 require 'calculator'
 
 describe Calculator do
-  
+  describe '#add' do
+    it 'will return 0 for an empty string input' do
+      calculator = Calculator.new
+      expect calculator.add("").to eq(0)
+    end
+  end
 end
