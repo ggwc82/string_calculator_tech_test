702849b689ddb9f87968474085a072eabfdacb7c
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 10:58:35 +0100

    First test passing for returning zero for empty string input

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,7 +4,7 @@ describe Calculator do
   describe '#add' do
     it 'will return 0 for an empty string input' do
       calculator = Calculator.new
-      expect calculator.add("").to eq(0)
+      expect(calculator.add("")).to eq(0)
     end
   end
 end
