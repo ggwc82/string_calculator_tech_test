d925c6f06cddbc52ff3ca639fd9536d0fa6ea337
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 11:18:16 +0100

    Refactored if/else conditional to guard clause if statement

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,7 @@
 class Calculator
   def add(input)
-    if input.length < 2
-      input.to_i
-    else
-      temp_array = input.split(",")
-      temp_array[0].to_i + temp_array[1].to_i
-    end
+    return input.to_i if input.length < 2
+    temp_array = input.split(",")
+    temp_array[0].to_i + temp_array[1].to_i
   end
 end
