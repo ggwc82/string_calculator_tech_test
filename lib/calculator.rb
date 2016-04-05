2665218be4b3dd7fa5e2f5de1e1cb4850805ad50
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 11:50:05 +0100

    Refactored - created new private methods to handle parsing string, predicate method for detecting incorrect_input? and also sum method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,21 @@
 class Calculator
-  def add(input)
-    cleaned_input = input.gsub("\n", ",")
-    raise ArgumentError, 'incorrect input format' if cleaned_input.include?(",,")
-    cleaned_input.split(",").map(&:to_i).inject(0, :+)
+  def add(string)
+    cleaned_input = parse_input(string)
+    raise ArgumentError, 'incorrect input format' if incorrect_input?(cleaned_input)
+    sum(cleaned_input)
+  end
+
+  private
+
+  def parse_input(string)
+    string.gsub("\n", ",")
+  end
+
+  def incorrect_input?(string)
+    string.include?(",,")
+  end
+
+  def sum(array)
+    array.split(",").map(&:to_i).inject(0, :+)
   end
 end
