a9b15a9778d8ed76af948700d69019d8d6f18acd
Godfrey Chiu <ggwc82@gmail.com>
2016-04-05 11:31:10 +0100

    Refactored to use inject method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,6 @@
 class Calculator
   def add(input)
-    temp_array = input.split(",")
-    sum = 0;
-    temp_array.each do |element|
-      sum += element.to_i
-    end
-    sum
+    temp_array = input.split(",").map { |element| element.to_i }
+    temp_array.inject(0, :+)
   end
 end
